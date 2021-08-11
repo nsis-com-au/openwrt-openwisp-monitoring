8766358bcea98e57d772d461f87246af78e14d18
Kapil Bansal <kapilbansal.gbpecdelhi@gmail.com>
2021-08-11 21:21:47 +0530

    [qa] Added shell formatter and shellcheck test

--- install-dev.sh
+++ install-dev.sh
@@ -3,18 +3,16 @@ set -e
 apt-get update
 # install cmake and git
 apt-get install -y cmake git
-# install shellcheck
-apt-get install shellcheck
 # install lua
 apt-get install -y lua5.1 liblua5.1-0-dev luarocks
 #install json-c
 git clone https://github.com/json-c/json-c.git --depth=1
-cd json-c && cmake . && make install && cd ..  || { echo 'Installing json-c failed!' ; exit 1; }
+cd json-c && cmake . && make install && cd .. || { echo 'Installing json-c failed!' && exit 1; }
 # install openwrt libubox and uci
 git clone https://git.openwrt.org/project/libubox.git --depth=1
-cd libubox && cmake . && make install && cd .. || { echo 'Installing libubox failed!' ; exit 1; }
+cd libubox && cmake . && make install && cd .. || { echo 'Installing libubox failed!' && exit 1; }
 git clone https://git.openwrt.org/project/uci.git --depth=1
-cd uci && cmake . && make install && cd .. || { echo 'Installing uci failed!' ; exit 1; }
+cd uci && cmake . && make install && cd .. || { echo 'Installing uci failed!' && exit 1; }
 #install nixio
 luarocks install https://raw.githubusercontent.com/Neopallium/nixio/master/nixio-scm-0.rockspec
 # update links to shared libraries
